a7756a6a9b958ba6e5720d7bb80cb0dcbca77741
Rogério Balloussier <rogerioballoussier@icloud.com>
2024-07-29 19:20:11 -0300

    trocando variáveis.

--- ponto_eletr.py
+++ ponto_eletr.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 
-def convert_time(hora: str) -> datetime:
+def converter_hora(hora: str) -> datetime:
     try:
         return datetime(
             year=datetime.now().year,
@@ -20,18 +20,18 @@ def convert_time(hora: str) -> datetime:
         )
 
 
-deu_entrada: datetime = convert_time(input("Que horas deu entrada? "))
-saiu_para_almoco: datetime = convert_time(input("Que horas saiu para almoço? "))
-voltou_do_almoco: datetime = convert_time(input("Que horas voltou do almoço? "))
+hora_entrada: datetime = converter_hora(input("Que horas deu entrada? "))
+saiu_para_almoco: datetime = converter_hora(input("Que horas saiu para almoço? "))
+voltou_do_almoco: datetime = converter_hora(input("Que horas voltou do almoço? "))
 
-print(f"\n{deu_entrada.strftime("%H:%M")} → a hora que bateu ponto de entrada.")
-print(f"{(deu_entrada + timedelta(hours=6, minutes=15)).strftime('%H:%M')} → a hora da saída normal (15 minutos).")
+print(f"\n{hora_entrada.strftime("%H:%M")} → a hora que bateu ponto de entrada.")
+print(f"{(hora_entrada + timedelta(hours=6, minutes=15)).strftime('%H:%M')} → a hora da saída normal (15 minutos).")
 
 if saiu_para_almoco.strftime("%H:%M") != "00:00" and voltou_do_almoco.strftime("%H:%M") != "00:00":
-    duration = voltou_do_almoco - saiu_para_almoco
+    duration: timedelta = voltou_do_almoco - saiu_para_almoco
     print(f"{duration.seconds / 60:.0f} minutos foi o tempo de duração de almoço.")
-    print(f"{(deu_entrada + timedelta(hours=6) + duration).strftime('%H:%M')} → a hora da saída sem hora extra.")
-    print(f"{(deu_entrada + timedelta(hours=7) + duration).strftime('%H:%M')} → a hora da saída com 1 hora extra.")
-    print(f"{(deu_entrada + timedelta(hours=8) + duration).strftime('%H:%M')} → a hora da saída com 2 horas extra.")
+    print(f"{(hora_entrada + timedelta(hours=6) + duration).strftime('%H:%M')} → a hora da saída sem hora extra.")
+    print(f"{(hora_entrada + timedelta(hours=7) + duration).strftime('%H:%M')} → a hora da saída com 1 hora extra.")
+    print(f"{(hora_entrada + timedelta(hours=8) + duration).strftime('%H:%M')} → a hora da saída com 2 horas extra.")
 else:
     print("Não deu ponto de entrada e saída do almoço...")
