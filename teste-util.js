405c9e4d0723ec380d76c2c8a8848ac5ae60e413
Rogério Balloussier <rogerioballoussier@icloud.com>
2024-08-21 22:00:35 -0300

    ...

--- teste-util.js
+++ teste-util.js
@@ -7,3 +7,16 @@ console.log(aa + bb);
 console.log(aa - bb);
 console.log(aa * bb);
 console.log(aa / bb);
+
+let frutas = ["Maçã", "Banana", "Laranja"];
+
+console.log(frutas);
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+
+frutas.push("Uva");
+console.log(frutas);
+
+frutas.pop();
+console.log(frutas);
