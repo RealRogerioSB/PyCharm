551c4af7eba31e867f9a6c79d8da6f234f5eaf26
Rogério <rogerioballoussier@icloud.com>
2024-12-18 21:11:53 -0300

    added commit

---
+++ heranca.py
@@ -0,0 +1,77 @@
+# %%
+# herança simples
+class Veiculo:
+    def __init__(self, cor, placa, numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_rodas = numero_rodas
+
+    def turn_engine(self):
+        print("Ligando o motor...")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+
+class Motocicleta(Veiculo):
+    pass
+
+
+class Carro(Veiculo):
+    pass
+
+
+class Caminhao(Veiculo):
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas)
+        self.carregado = carregado
+
+    def esta_carregado(self):
+        print(f"{'Sim' if self.carregado else 'Não'} estou carregado.")
+
+
+moto = Motocicleta("preta", "abc-1234", 2)
+carro = Carro("branco", "xde-0098", 4)
+caminhao = Caminhao("roxo", "gfd-8712", 8, True)
+
+print(moto)
+print(carro)
+print(caminhao)
+
+
+# %%
+# herança múltipla
+class Animal:
+    def __init__(self, nro_patas):
+        self.nro_patas = nro_patas
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+
+class Mamifero(Animal):
+    def __init__(self, cor_pelo, **kw):
+        super().__init__(**kw)
+        self.cor_pelo = cor_pelo
+
+
+class Ave(Animal):
+    def __init__(self, cor_bico, **kw):
+        super().__init__(**kw)
+        self.cor_bico = cor_bico
+
+
+class Gato(Mamifero):
+    pass
+
+
+class Ornitorrinco(Mamifero, Ave):
+    def __init__(self, cor_bico, cor_pelo, nro_patas):
+        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
+
+
+gato = Gato(nro_patas=4, cor_pelo="Preto")
+print(gato)
+
+ornitorrinco = Ornitorrinco(nro_patas=2, cor_pelo="vermelho", cor_bico="laranja")
+print(ornitorrinco)
