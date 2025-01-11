0a35b2b0fe9c8cf507a7c7190a089b6d8b88299c
Rogério <rogerioballoussier@icloud.com>
2025-01-11 11:22:53 -0300

    updated commit

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ load_dotenv()
 engine = sa.create_engine(os.getenv("URL_AIVEN"))
 
 df_new = [
+    {"id_curso": 202405, "nm_curso": "Flask: crie uma webapp com Python", "hr_curso": 10},
+    {"id_curso": 202471, "nm_curso": "Flask: avançando no desenvolvimento web com Python", "hr_curso": 10},
+    {"id_curso": 202530, "nm_curso": "Git e GitHub: repositório, commit e versões", "hr_curso": 8},
+    {"id_curso": 201034, "nm_curso": "Swagger parte 1: crie uma documentação APIs REST", "hr_curso": 8},
+    {"id_curso": 201062, "nm_curso": "Swagger parte 2: customizando uma API gerada", "hr_curso": 15},
     {"id_curso": 201694, "nm_curso": "Criação de valor: conhecendo os conceitos-chave para gerenciamento de serviços", "hr_curso": 8},
     {"id_curso": 201696, "nm_curso": "Cadeia de valor de serviços: conhecendo as 4 dimensões", "hr_curso": 6},
     {"id_curso": 202525, "nm_curso": "Estratégia Omnichannel: aumentando suas vendas", "hr_curso": 8},
