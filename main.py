b67530a5f9c3571ec7d1b54fe524bf8d6ba8ac69
Rogério <rogerioballoussier@icloud.com>
2025-01-13 19:41:15 -0300

    updated commit

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ load_dotenv()
 engine = sa.create_engine(os.getenv("URL_AIVEN"))
 
 df_new = [
-    # {"id_curso": 201034, "hr_curso": 8, "nm_curso": "Swagger parte 1: crie uma documentação APIs REST"},
-    # {"id_curso": 201062, "hr_curso": 15, "nm_curso": "Swagger parte 2: customizando uma API gerada"},
-    # {"id_curso": 203945, "hr_curso": 6, "nm_curso": "Engenharia de Prompt: criando prompts eficazes para IA Generativa"},
+    {"id_curso": 201062, "hr_curso": 15, "nm_curso": "Swagger parte 2: customizando uma API gerada"},
 ]
 
 exp = pd.DataFrame(df_new).to_sql(name="unibb", con=engine, if_exists="append", index=False)
