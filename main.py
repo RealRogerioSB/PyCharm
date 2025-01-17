9a6ef8be749c27a059994f90b806fcf6ed10bce0
Rogério Balloussier <143833175+RealRogerioSB@users.noreply.github.com>
2025-01-17 09:32:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,69 @@
-from datetime import datetime, timedelta, UTC
+from datetime import date
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from pydantic import BaseModel
+
+app: FastAPI = FastAPI()
+
+fake_db: list[dict[str: bool | date | int | str]] = [
+    {"_id": 1, "_title": "Criando uma aplicação com Django", "_date": date.today(), "_published": True},
+    {"_id": 2, "_title": "Criando uma aplicação com Flask", "_date": date.today(), "_published": True},
+    {"_id": 3, "_title": "Criando uma aplicação com FastAPI", "_date": date.today(), "_published": False},
+    {"_id": 4, "_title": "Criando uma aplicação com Starlette", "_date": date.today(), "_published": False},
+]
 
 
 class Post(BaseModel):
     _id: int
-    _title: str
-    _date: datetime = datetime.now(UTC)
-    _published: bool = True
+    title: str
+    date: date = date.today()
+    published: bool = True
 
 
-list_post = [
-    {"_id": 1, "_title": "Aplicação de FastAPI", "_date": datetime.now(UTC), "_published": True},
-    {"_id": 2, "_title": "Aplicação de Django", "_date": datetime.now(UTC) - timedelta(days=365), "_published": False},
-    {"_id": 3, "_title": "Aplicação de Flask", "_date": datetime.now(UTC) - timedelta(weeks=9), "_published": True},
-    {"_id": 4, "_title": "Aplicação de Starlette", "_date": datetime.now(UTC) - timedelta(hours=44), "_published": False},
-]
-
-app = FastAPI()
+@app.get("/post", tags=["Post"], status_code=status.HTTP_200_OK)
+async def list_post() -> list:
+    return fake_db
 
 
-@app.get(path="/")
-async def root() -> dict:
-    return {"mensagem": "Página Raíz"}
+@app.get("/post/disponiveis", tags=["Post"], status_code=status.HTTP_200_OK)
+async def list_post_disponiveis() -> list:
+    return [fake for fake in fake_db if fake["_published"]]
 
 
-@app.get(path="/posts")
-async def listar_posts() -> list:
-    return list_post
+@app.get("/post/{item}", tags=["Post"], status_code=status.HTTP_200_OK)
+async def get_item_post(item: int) -> dict:
+    for _item in fake_db:
+        if _item["_id"] == item:
+            return _item
+    return {"mensagem": "Não foi possível localizar o item..."}
 
 
-@app.get(path="/posts/disponiveis")
-async def listar_disponiveis() -> list:
-    return [post for post in list_post if post["_published"]]
-
-
-@app.get(path="/posts/{item}")
-async def obter_posts(item: int) -> dict:
-    for post in list_post:
-        if post["_id"] == item:
-            return post
-    return {"mensagem": "Não foi encontrado o item..."}
-
-
-@app.post(path="/posts")
-async def create_post(_new: dict) -> dict:
-    list_post.append(_new)
+@app.post("/post", tags=["Post"], status_code=status.HTTP_201_CREATED)
+async def create_post(fake: dict) -> dict:
+    fake_db.append(fake)
     return {"mensagem": "Post criado com sucesso!"}
 
 
-@app.put(path="/posts/{item}")
-async def update_post(item: int, _post: dict) -> dict:
-    for post in list_post:
-        if post["_id"] == item:
-            post["_title"] = _post["_title"]
-            post["_published"] = _post["_published"]
+@app.put("/post/{item}", tags=["Post"], status_code=status.HTTP_200_OK)
+async def update_post(item: int, _fake: dict) -> dict:
+    for fake in fake_db:
+        if fake["_id"] == item:
+            fake["_title"] = _fake["_title"]
+            fake["_date"] = _fake["_date"]
+            fake["_published"] = _fake["_published"]
             return {"mensagem": "Post atualizado com sucesso!"}
-    return {"mensagem": "Não foi encontrado o item..."}
+    return {"mensagem": "Não foi possível localizar o item..."}
 
 
-@app.delete(path="/posts/{item}")
+@app.delete("/post/{item}", tags=["Post"], status_code=status.HTTP_200_OK)
 async def delete_post(item: int) -> dict:
-    for index, post in enumerate(list_post):
-        if post["_id"] == item:
-            list_post.pop(index)
-            return {"mensagem": "Post removido com sucesso!"}
-    return {"mensagem": "Não foi encontrado o item..."}
+    for index, _item in enumerate(fake_db):
+        if _item["_id"] == item:
+            fake_db.pop(index)
+            return {"mensagem": "Post deletado com sucesso!"}
+    return {"mensagem": "Não foi possível localizar o item..."}
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     uvicorn.run(app="main:app", port=5000)
